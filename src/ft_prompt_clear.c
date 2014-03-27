7921d88c348293d2b4347fc204d79a2265939523
Guillaume CHATEAUX <gchateau@e3r13p5.42.fr>
2014-03-27 18:57:52 +0100

    ft_prompt_clear norme

--- src/ft_prompt_clear.c
+++ src/ft_prompt_clear.c
@@ -6,7 +6,7 @@
 /*   By: gchateau <gchateau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/06 02:10:08 by gchateau          #+#    #+#             */
-/*   Updated: 2014/03/24 18:09:27 by gchateau         ###   ########.fr       */
+/*   Updated: 2014/03/27 18:57:33 by gchateau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_prompt_clear(t_datas *datas, t_line *line)
 {
 	if (line->len == 0)
 		return ;
-	if (line->i < line->len -1)
+	if (line->i < line->len - 1)
 		ft_move_end(datas, line, FTSH_KEY_END);
 	while (!(line->x == 0 && line->y == 0))
 	{
